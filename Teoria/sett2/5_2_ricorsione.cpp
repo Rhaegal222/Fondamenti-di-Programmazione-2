533304d362347d0274d4a0451c08b0dc1de725ca
Francesco Vecchio <rhaegal222@gmail.com>
2022-10-03 09:07:24 +0200

    update Fondamenti-di-Programmazione-2

--- Teoria/sett2/5_2_ricorsione.cpp
+++ Teoria/sett2/5_2_ricorsione.cpp
@@ -1,21 +1,17 @@
 #include <iostream>
 using namespace std;
 
-
 int fact(int val){
 	if (val <= 1) return 1;
 	else return fact (val-1) * val;
 }
 
-
 int main(){
 	int val;
 	cin >> val;
 	cout << fact(val) << endl;
-
 	return 0;
 }
-
 
 #if 0
 //CHIAMATA		RETURN 					VALORE
@@ -24,5 +20,4 @@ fact(4)			fact(3) * 4				24 	^
 fact(3)			fact(2) * 3				6 	^
 fact(2)			fact(1) * 2				2 	^
 fact(1)			1						1 	^
-
 #endif
