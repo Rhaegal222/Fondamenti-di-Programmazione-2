c7909ac36e60b10cebba925844ac820c3a02c829
Francesco Vecchio <rhaegal222@gmail.com>
2023-02-09 17:29:41 +0100

    update esercizio4.2.cpp

--- Tracce-Appelli/24-01-2023/esercizio4.2.cpp
+++ Tracce-Appelli/24-01-2023/esercizio4.2.cpp
@@ -5,13 +5,13 @@ using namespace std;
 
 bool isComplete(vector<vector<int>>& S, vector<int>& H, int& k){
     for(auto x:H) cout<<x<< " "; cout<<endl;
-    if(H.size() < k) return false;
+    if(H.size() < k) return false; //se la combinazione non è della dimensione richiesta (k) la condizione non è rispettata
     for(auto x:S){ //scorre il vettore delle stringhe
         bool cond = false; 
         for(auto y:H) if(find(x.begin(), x.end(), y)!=x.end()) cond = true; //scorre la combinazione e controlla che il carattere selezionato è presente nella stringa
         if(!cond) return false; //se non è presente almeno un carattere della combinazione nella sottostringa restituisce falso
     }
-    return true; //la condizione è verificata
+    return true; //le condizioni sono verificate
 }
 
 bool esercizio4(vector<vector<int>>& S, vector<int>& H, int& k, int& n, int index){
@@ -28,8 +28,6 @@ int main(){
     vector<vector<int>> S{{1, 2, 3}, {4, 3, 5}, {5, 6}};
     vector<int> H;
     int k = 3, n = 5;
-
     if(esercizio4(S,H,k,n,0)) cout << "SI"; else cout << "NO"; cout << endl;
-
     return 0;
 }
